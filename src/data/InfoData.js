486f65756ca47b64ab89d30a638c1d17b74181e5
brainard23 <aidenyale00@gmail.com>
2022-01-24 14:44:27 +0800

    real estate

---
+++ src/data/InfoData.js
@@ -0,0 +1,24 @@
+import ImageOne from '../images/ImageFive.jpg'
+import ImageTwo from '../images/ImageSix.jpg'
+
+export const InfoData = {
+    header: 'Explore our Beautiful homes',
+    paragraphOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis egestas. Ornare arcu odio ut sem nulla pharetra diam. A erat nam at lectus urna. Commodo viverra maecenas accumsan lacus vel facilisis volutpat. ',
+    paragraphTwo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cras fermentum odio eu feugiat pretium nibh ipsum consequat. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed.',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+
+};
+
+export const InfoDataTwo = {
+    header: 'Explore our Beautiful modern homes',
+    paragraphOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis egestas. Ornare arcu odio ut sem nulla pharetra diam.  ',
+    paragraphTwo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cras fermentum odio eu feugiat pretium nibh ipsum consequatd.',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+
+}
---
+++ src/data/SliderData.js
@@ -0,0 +1,40 @@
+import ImageOne from '../images/ImageOne.jpg'
+import ImageTwo from '../images/ImageTwo.jpg'
+import ImageThree from '../images/ImageThree.jpg'
+import ImageFour from '../images/ImageFour.jpg'
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Bali, Indonesia', 
+        price: '$4,200,00',
+        path: '/homes',
+        label: 'View Home', 
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Costa Rica', 
+        price: '$2,800,00',
+        path: '/homes',
+        label: 'View Home', 
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasas, California', 
+        price: '$8,200,00',
+        path: '/homes',
+        label: 'View Home', 
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'House in Philippines, Cagayan de oro', 
+        price: '$200,00',
+        path: '/homes',
+        label: 'View Home', 
+        image: ImageFour,
+        alt: 'House'
+    },
+
+]
